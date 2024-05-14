b05364caffaf2f4db0e0a1d9352b672b48715165
Alex <alexander.christianson@outlook.com>
2024-05-14 23:52:10 +0200

    Tog bort lite onödiga kommentarer, rensade upp metoder i ApiClient

--- ApiClient.cs
+++ ApiClient.cs
@@ -5,7 +5,7 @@ namespace GenerateDishesAPI
 	public class ApiClient
 	{
 		private readonly HttpClient _httpClient;
-		private string url = "https://localhost:7231";
+		private readonly string _url = "https://localhost:7231";
 
 		public ApiClient()
 		{
@@ -46,19 +46,21 @@ namespace GenerateDishesAPI
 		{
 			ApiClient client = new ApiClient();
 			List<valuePair> kvps = new List< valuePair>();
-			string[] dishes = await client.GenerateDishesAsync($"https://localhost:7231/ChatAi");
-			string path = ($"https://localhost:7231/img");
+			string path = ($"{_url}/img");
+			valuePair kvp;
+			int counter = 0;
 
-			valuePair kvp = new valuePair();
-			int counter = 1;
+			//Get 10 dish-names from method that calls ChatAi
+			string[] dishes = await client.GenerateDishesAsync($"{_url}/ChatAi");
+			
 			foreach (string dish in dishes)
 			{
 				//Make GET request to Picture-API
 				string requestUrl = $"{path}?imgQuery={Uri.EscapeDataString(dish)}";
-
 				HttpResponseMessage response = await _httpClient.GetAsync(requestUrl);
-
 				string url = await response.Content.ReadAsStringAsync();
+
+				//Save dishes with respective picture as valuePair-object and add to list
 				kvp = new valuePair()
 				{
 					Id = counter++,
--- Program.cs
+++ Program.cs
@@ -77,26 +77,29 @@ namespace GenerateDishesAPI
 				return await client.GeneratePicturesAndDishesAsync();
 			});
 
-			app.MapPost("GenerateRecipe", (string dish) =>
+			app.MapPost("GenerateRecipe", async (OpenAIAPI api, string dish) =>
 			{
+				string query = $"print the ingredients and cooking instructions for dish: {dish}";
 
+				var chat = api.Chat.CreateConversation();
+
+				//Something went wrong here so its a bit different from how i did it but it still works
+				chat.Model = OpenAI_API.Models.Model.ChatGPTTurbo;
+
+				chat.RequestParameters.Temperature = 1;
+
+				chat.AppendSystemMessage("You are a food recepie database and will give out food recepies");
+				//Lägg till appends för json
+				chat.AppendUserInput(query);
+				chat.AppendExampleChatbotOutput("Beef wellington, Homemade pizza, Spaghetti carbonara, Birria tacos, Meatloaf, Fried chicken sandwich, Pulled pork, Escargot, Sushi, Mango sticky rice");
+				chat.AppendUserInput(query);
+				var answer = await chat.GetResponseFromChatbotAsync();
+
+				return answer;
 			});
-			//TwoDishesTwoPictures();
 
 			app.Run();
 		}
-
-		//static async void TwoDishesTwoPictures()
-		//{
-		//	ApiClient client = new ApiClient();
-		//	IDictionary<string, string> kvps = await client.GeneratePicturesAsync();
-
-		//	foreach (var kv in kvps)
-		//	{
-		//		await Console.Out.WriteLineAsync(kv.Key + " " + kv.Value);
-		//	}
-			
-		//}
 
 		//Namn email, id, lösen
 	}
