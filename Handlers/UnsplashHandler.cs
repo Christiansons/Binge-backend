2a8ecef8f53eaafb5208c22d324233847ffb2e00
Christiansons <alexander.christianson@outlook.com>
2024-06-03 18:34:28 +0200

    Added test-image if out of images on unsplash-api

--- Handlers/UnsplashHandler.cs
+++ Handlers/UnsplashHandler.cs
@@ -7,6 +7,11 @@ namespace GenerateDishesAPI.Handlers
 		public async Task<string> GeneratePictureUrlAsync(string dishName)
 		{
 			UnsplasharpClient client = new UnsplasharpClient("03jvn6lavoGJKCLyVN_Tw1GR654rFZbZbfVqRb6qiCE");
+
+			if (client.RateLimitRemaining == 0)
+			{
+				return "https://media.npr.org/assets/img/2019/09/27/nuts-1_custom-4d5a40d1dfed103fe42fed775960a3e15a27627c.jpg?s=1300&c=85&f=webp";
+			}
 			var photo = await client.SearchPhotos(dishName);
 
 			var url = photo.First().Urls.Regular;
