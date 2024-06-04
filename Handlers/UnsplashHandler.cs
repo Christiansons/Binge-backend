ea5d975ee3c51257795c4c437e977553adff164c
Alex <alexander.christianson@outlook.com>
2024-06-04 07:33:14 +0200

    Removed the demo nuts picture, when out of api calls

--- Handlers/UnsplashHandler.cs
+++ Handlers/UnsplashHandler.cs
@@ -8,10 +8,10 @@ namespace GenerateDishesAPI.Handlers
 		{
 			UnsplasharpClient client = new UnsplasharpClient("03jvn6lavoGJKCLyVN_Tw1GR654rFZbZbfVqRb6qiCE");
 
-			if (client.RateLimitRemaining == 0)
-			{
-				return "https://media.npr.org/assets/img/2019/09/27/nuts-1_custom-4d5a40d1dfed103fe42fed775960a3e15a27627c.jpg?s=1300&c=85&f=webp";
-			}
+			//if (client.RateLimitRemaining == 0)
+			//{
+			//	return "https://media.npr.org/assets/img/2019/09/27/nuts-1_custom-4d5a40d1dfed103fe42fed775960a3e15a27627c.jpg?s=1300&c=85&f=webp";
+			//} 
 			var photo = await client.SearchPhotos(dishName);
 
 			var url = photo.First().Urls.Regular;
