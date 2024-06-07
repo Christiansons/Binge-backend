c190d70ff8bfda6ad4bcc613f7293ddf0e8de998
Alex <alexander.christianson@outlook.com>
2024-06-07 13:54:26 +0200

    Added test-project

---
+++ GenerateDishesAPITests/ApiClientTests.cs
@@ -0,0 +1,28 @@
+using GenerateDishesAPI;
+using GenerateDishesAPI.Repositories;
+
+namespace GenerateDishesAPITests
+{
+	[TestClass]
+	public class ApiClientTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			
+		}
+		
+		[TestMethod]
+		public void GetDishesAsync_Gets10DishesAndReturnsThemCorrectlyFormated()
+		{
+			//Mock-svar på 10 rätter, se att dem tas in rätt
+			
+		}
+
+		[TestMethod]
+		public void GetPicturesAndDishesAsync_GetsPicturesAndDishes()
+		{
+
+		}
+	}
+}
